0ac81cdf3ce538258dbbf1b87633bcf29b2c6b53
Alyx24 <141672864+Alyx24@users.noreply.github.com>
2023-10-23 12:12:09 -0700

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+#include <iostream>
+
+//practice
+
+#include <iostream>
+
+int bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap the elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    return 0; // Return 0 to indicate success.
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int result = bubbleSort(arr, n);
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
